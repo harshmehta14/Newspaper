fb7a886468629f670930f4e288ddff116d449526
harshmehta14 <harshymehta@gmail.com>
2022-11-12 23:20:20 +0530

    backend in progress

---
+++ backend/models/paperboy_schema.js
@@ -0,0 +1,100 @@
+const mongoose = require('mongoose')
+
+// const users_schema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique:true
+//   },
+//   phno: {
+//     type: String,
+//     required: true
+//   },
+//   address: {
+//     type: String,
+//     required: true
+//   },
+//   items: {
+//     type: Object,
+//   },
+//   signup_date: {
+//     type: Date,
+//     required: true,
+//     default: Date.now
+//   },
+//   stop:{
+//     type:Boolean,
+//     default:false,
+//   }
+// })
+
+
+const paperboy_schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phno: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  dob:{
+    type: Date,
+  },
+  signup_date: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  stop:{
+    type:Boolean,
+    default:false,
+  },
+  distributor:{
+    type:String,
+  }
+})
+
+// const distributor_schema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true
+//   },
+//   email: {
+//     type: String,
+//     required: true
+//   },
+//   phno: {
+//     type: String,
+//     required: true
+//   },
+//   address: {
+//     type: String,
+//     required: true
+//   },
+//   signup_date: {
+//     type: Date,
+//     required: true,
+//     default: Date.now
+//   },
+//   newspaper_variety:{
+//     type:Array,
+    
+//   }
+// })
+
+// module.exports = mongoose.model('users', users_schema)
+module.exports = mongoose.model('paperboy', paperboy_schema)
+// module.exports = mongoose.model('distributor', distributor_schema)
--- backend/routes/crud.js
+++ backend/routes/crud.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const router = express.Router()
-const users = require('../models/schema')
-const paperboy = require('../models/schema')
-const distributor = require('../models/schema')
+const users = require('../models/user_schema')
+const paperboy = require('../models/paperboy_schema')
+// const distributor = require('../models/schema')
+
 
 
 router.get('/', async (req, res) => {
   try {
     const all_users = await users.find()
-    res.json(all_user)
+    res.json(all_users)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+
+router.get('/paperboy', async (req, res) => {
+  try {
+    const all_users = await paperboy.find()
+    res.json(all_users)
   } catch (err) {
     res.status(500).json({ message: err.message })
   }
@@ -26,40 +37,40 @@ router.post('/', async (req, res) => {
     const newuser = await user.save()
     res.status(201).json(newuser)
   } catch (err) {
-    res.status(400).json({ message: "err.message" })
+    res.status(400).json({ message: err.message })
   }
 })
 
 
 router.post('/paperboy', async (req, res) => {
-  const user = new paperboy({
+  const user_paperboy = new paperboy({
     name: req.body.name,
     email: req.body.email,
     phno:  req.body.phno,
     address:  req.body.address,
   })
   try {
-    const newuser = await user.save()
-    res.status(201).json(newuser)
+    const newuser_paper = await user_paperboy.save()
+    res.status(201).json(newuser_paper)
   } catch (err) {
     res.status(400).json({ message: err.message })
   }
 })
 
-router.post('/distributor', async (req, res) => {
-  const user = new distributor({
-    name: req.body.name,
-    email: req.body.email,
-    phno:  req.body.phno,
-    address:  req.body.address,
-  })
-  try {
-    const newuser = await user.save()
-    res.status(201).json(newuser)
-  } catch (err) {
-    res.status(400).json({ message: err.message })
-  }
-})
+// router.post('/distributor', async (req, res) => {
+//   const user = new distributor({
+//     name: req.body.name,
+//     email: req.body.email,
+//     phno:  req.body.phno,
+//     address:  req.body.address,
+//   })
+//   try {
+//     const newuser = await user.save()
+//     res.status(201).json(newuser)
+//   } catch (err) {
+//     res.status(400).json({ message: err.message })
+//   }
+// })
 
 
 module.exports = router
