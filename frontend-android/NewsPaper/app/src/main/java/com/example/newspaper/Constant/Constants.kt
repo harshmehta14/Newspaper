63ecc3fb2c9eab4fb39bdd3927a77485cfbb1bcc
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-11-12 15:42:12 +0530

    newsapi done

--- frontend-android/NewsPaper/app/src/main/java/com/example/newspaper/Constant/Constants.kt
+++ frontend-android/NewsPaper/app/src/main/java/com/example/newspaper/Constant/Constants.kt
@@ -2,4 +2,5 @@ package com.example.newspaper.Constant
 
 object Constants {
     const val GETNEWS = "https://newsapi.org/v2/everything?q=twitter&from=2022-10-12&sortBy=publishedAt&apiKey=5b846973dc6f44f38c6a6ff7ba24a2c8"
+    const val DISTRIBUTOR = "distributor"
 }
--- frontend-android/NewsPaper/app/src/main/java/com/example/newspaper/presentation/newsApi/DigitalNewsScreen.kt
+++ frontend-android/NewsPaper/app/src/main/java/com/example/newspaper/presentation/newsApi/DigitalNewsScreen.kt
@@ -1,15 +1,18 @@
 package com.example.newspaper.presentation.newsApi
 
 import androidx.activity.ComponentActivity
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.rememberImagePainter
 
 @Composable
 fun NewsApiScreen(viewModel: NewsApiViewModel = hiltViewModel(),context:ComponentActivity) {
@@ -24,7 +27,53 @@ fun NewsApiScreen(viewModel: NewsApiViewModel = hiltViewModel(),context:Componen
 
         if (viewModel.news.value!=null){
             viewModel.news.value?.articles?.forEach {
-
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(10.dp)),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        if (it.urlToImage==null){
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(100.dp)
+                                    .clip(RoundedCornerShape(20.dp)),
+                                horizontalArrangement = Arrangement.Center,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text(text = "No image")
+                            }
+                        }
+                        else{
+                            Image(
+                                painter = rememberImagePainter(data = it.urlToImage),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(100.dp)
+                                    .clip(RoundedCornerShape(20.dp))
+                            )
+                        }
+                        Spacer(modifier = Modifier.padding(15.dp))
+                        Text(text = it.title)
+                        Spacer(modifier = Modifier.padding(10.dp))
+                        Text(text = it.description)
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Start,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(text = "Author :"+it.author)
+                        }
+                    }
+                }
             }
         }
     }
